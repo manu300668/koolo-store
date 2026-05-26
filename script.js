d9c43b2fc3984d74b8236fa6e3388b996c047a53
manu300668 <eumanu@msn.com>
2026-05-26 17:20:00 +0200

    Integrate Firebase for product data retrieval
    Added Firebase configuration and initialization. Implemented Firestore data retrieval and display logic.

--- script.js
+++ script.js
@@ -1,11 +1,84 @@
+// CONFIG FIREBASE
+
+const firebaseConfig = {
+
+apiKey: "TU_API_KEY",
+authDomain: "TU_PROYECTO.firebaseapp.com",
+projectId: "TU_PROYECTO",
+storageBucket: "TU_PROYECTO.appspot.com",
+messagingSenderId: "XXXX",
+appId: "XXXX"
+
+};
+
+
+// INICIAR FIREBASE
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+
+// TEST
+
 document.addEventListener("DOMContentLoaded", () => {
 
 const productos = document.getElementById("productosContainer");
 
+productos.innerHTML = "<h2>Conectando con Firebase...</h2>";
+
+db.collection("productos")
+
+.get()
+
+.then(snapshot => {
+
+if(snapshot.empty){
+
 productos.innerHTML = `
 
-<h2>JavaScript funcionando</h2>
+<h2>No hay productos en Firebase</h2>
 
 `;
+
+return;
+
+}
+
+productos.innerHTML = "<h2>Firebase conectado ✅</h2>";
+
+snapshot.forEach(doc => {
+
+const p = doc.data();
+
+productos.innerHTML += `
+
+<div style="padding:20px;border:1px solid #ccc;margin:20px;background:white">
+
+<h3>${p.nombre}</h3>
+
+<p>${p.precio} €</p>
+
+</div>
+
+`;
+
+});
+
+})
+
+.catch(error => {
+
+productos.innerHTML = `
+
+<h2>Error Firebase ❌</h2>
+
+<p>${error.message}</p>
+
+`;
+
+console.error(error);
+
+});
 
 });
