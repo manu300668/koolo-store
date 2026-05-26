8b810ac65f4d2a50a754254d3d896bd0d8e27787
manu300668 <eumanu@msn.com>
2026-05-26 17:21:05 +0200

    Replace Firebase config with new project details
    Updated Firebase configuration with new credentials.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// CONFIG FIREBASE
+un// CONFIG FIREBASE
 
 const firebaseConfig = {
 
-apiKey: "TU_API_KEY",
-authDomain: "TU_PROYECTO.firebaseapp.com",
-projectId: "TU_PROYECTO",
-storageBucket: "TU_PROYECTO.appspot.com",
-messagingSenderId: "XXXX",
-appId: "XXXX"
+apiKey:"AIzaSyA7orSwoJ5XcDd-ABRj_o_OVySCdC05mxE",
+authDomain:"koolo2026.firebaseapp.com",
+projectId:"koolo2026",
+storageBucket:"koolo2026.appspot.com",
+messagingSenderId:"612039099714",
+appId:"1:612039099714:web:38d14c2de43851c225c74db"
 
 };
 
