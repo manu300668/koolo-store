797749ca2c18f1e750561a8935cedbd6b6aaf6b9
manu300668 <eumanu@msn.com>
2026-05-27 13:30:10 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,145 @@
+// FIREBASE CONFIG
+
+const firebaseConfig = {
+
+apiKey: "TU_API_KEY",
+
+authDomain: "TU_PROYECTO.firebaseapp.com",
+
+projectId: "TU_PROYECTO",
+
+storageBucket: "TU_PROYECTO.appspot.com",
+
+messagingSenderId: "XXXX",
+
+appId: "XXXX"
+
+};
+
+
+// INICIAR FIREBASE
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+
+// CUANDO CARGUE LA PÁGINA
+
+document.addEventListener("DOMContentLoaded", () => {
+
+const productosContainer =
+document.getElementById("productosContainer");
+
+
+// MENSAJE CARGANDO
+
+productosContainer.innerHTML = `
+
+<h2>Cargando productos...</h2>
+
+`;
+
+
+// LEER PRODUCTOS FIRESTORE
+
+db.collection("productos")
+
+.get()
+
+.then(snapshot => {
+
+productosContainer.innerHTML = "";
+
+
+// SI NO HAY PRODUCTOS
+
+if(snapshot.empty){
+
+productosContainer.innerHTML = `
+
+<h2>No hay productos disponibles</h2>
+
+`;
+
+return;
+
+}
+
+
+// CREAR PRODUCTOS
+
+snapshot.forEach(doc => {
+
+const producto = doc.data();
+
+productosContainer.innerHTML += `
+
+<div class="producto">
+
+<img src="${producto.imagen}" alt="${producto.nombre}">
+
+<h3>${producto.nombre}</h3>
+
+<p>${producto.precio} €</p>
+
+<a href="product.html?id=${doc.id}">
+
+<button>Ver producto</button>
+
+</a>
+
+</div>
+
+`;
+
+});
+
+})
+
+.catch(error => {
+
+console.error(error);
+
+productosContainer.innerHTML = `
+
+<h2>Error Firebase</h2>
+
+<p>${error.message}</p>
+
+`;
+
+});
+
+});
+
+
+
+
+// CARRITO SIMPLE
+
+function abrirCarrito(){
+
+document
+.getElementById("carritoPanel")
+.classList.remove("oculto");
+
+}
+
+
+function cerrarCarrito(){
+
+document
+.getElementById("carritoPanel")
+.classList.add("oculto");
+
+}
+
+
+function vaciarCarrito(){
+
+localStorage.removeItem("carrito");
+
+location.reload();
+
+}
