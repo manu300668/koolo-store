caa77e1c57a6b578a0ee61cd018517f0a4bad9f0
manu300668 <eumanu@msn.com>
2026-05-28 07:23:37 +0200

    Update appId in Firebase configuration

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ authDomain:"koolo2026.firebaseapp.com",
 projectId:"koolo2026",
 storageBucket:"koolo2026.appspot.com",
 messagingSenderId:"612039099714",
-appId:"1:612039099714:web:
+appId:"1:612039099714:web:38d14c2de43851c225c74db"
 
 };
 
