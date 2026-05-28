7003063900ccecb9aef54d172860b185a96bb423
manu300668 <eumanu@msn.com>
2026-05-28 07:06:09 +0200

    Update Firebase API key in script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 
 const firebaseConfig = {
 
-apiKey:"TU_API_KEY",
+apiKey:"AIzaSyA7orSwoJ5XcDd-ABRj_o_OVySCdC05mxE",
 authDomain:"koolo2026.firebaseapp.com",
 projectId:"koolo2026",
 storageBucket:"koolo2026.appspot.com",
 messagingSenderId:"612039099714",
-appId:"1:612039099714:web:38d14c2de43851c225c74db"
+appId:"1:612039099714:web:
 
 };
 
