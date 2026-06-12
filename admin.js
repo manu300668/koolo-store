2fbd11465fad669f9deefd43ae9dd8175d32ae4e
manu300668 <eumanu@msn.com>
2026-06-12 18:44:06 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -26,9 +26,28 @@ let productoEditando = null;
 // ==========================
 
 function login(){
-alert("LOGIN FUNCIONA");
+
+const user =
+document.getElementById("user").value;
+
+const pass =
+document.getElementById("pass").value;
+
+if(user === "koolo" && pass === "Manu68."){
+
+document.getElementById("loginBox").style.display = "none";
+
+document.getElementById("adminPanel").style.display = "block";
+
+cargarAdmin();
+
+}else{
+
+alert("Error login");
+
 }
 
+}
 // ==========================
 // CARGAR PANEL
 // ==========================
